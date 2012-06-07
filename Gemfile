c31dec5b0276cb7d46761921154b8c592ced62a2
Michael D. Hall <mdh@just3ws.com>
2012-06-07 07:50:33 -0500

    Initial Rails-Api application w/RSpec

---
+++ Gemfile
@@ -0,0 +1,9 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.5'
+gem 'jbuilder'
+gem 'chronic'
+
+group :test, :development do
+  gem 'rspec-rails'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UglstDtsrv::Application.config.secret_token = 'c9b744d935aeae67e35232d7eb797fc56c9f8357dca9be0789d4a7f48490cce7dc234ac77eb1ed387e17e43c5df4b885c183dfc703e03e77af39d7697cacce1e'
