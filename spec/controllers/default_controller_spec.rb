42de779cbf9859235e44037b71958ff7b9d323f2
Michael D. Hall <mdh@just3ws.com>
2012-06-08 07:28:20 -0500

    Added specs for parsing date and ensuring a valid result

--- spec/controllers/default_controller_spec.rb
+++ spec/controllers/default_controller_spec.rb
@@ -9,4 +9,28 @@ describe DefaultController do
     end
   end
 
+  describe "GET 'parse_date'" do
+    render_views
+
+    it "returns a valid date" do
+
+      date = "tomorrow"
+
+      get 'parse', {q: date}
+
+      body = JSON(response.body)
+
+      p body
+
+      body["q"].should == date
+
+      body["valid"].should        be_true
+      body["utc"].should_not      be_empty
+      body["local"].should_not    be_empty
+      body["offset"].should_not   be_nil
+      body["timezone"].should_not be_empty
+
+      response.should be_success
+    end
+  end
 end
