bc5a6b89798b42bc979c180a66f3db28d97c270f
Michael D. Hall <mdh@just3ws.com>
2012-06-07 22:16:14 -0500

    Added Local TimeZone offset support
    Can now include an offset for local time with
    request and receive back the time offset from UTC.
    
    Also clean up input, strip and downcase.

--- app/controllers/default_controller.rb
+++ app/controllers/default_controller.rb
@@ -2,9 +2,31 @@ require 'chronic'
 
 class DefaultController < ApplicationController
   def parse
-    @q = params[:q] || Time.now.utc.to_s
+    @q      = (params[:q] || Time.now.utc.to_s).strip.downcase
+    @offset = Integer(params[:offset] || 0)
+
     Chronic.time_class = ActiveSupport::TimeZone["UTC"]
-    @utc = Chronic.parse(@q)
+
+    @utc      = Chronic.parse(@q)
+    @local    = get_local_time_from_offset(@utc, @offset)
+    @timezone = get_timezone_name_from_offset(@offset)
+
     render 'parse', formats: 'json'
+  end
+
+  private
+
+  def get_local_time_from_offset(utc, offset)
+    get_timzeone_from_offset(offset).at(utc)
+  end
+
+  def get_timzeone_from_offset(offset = 0)
+    ActiveSupport::TimeZone[ get_timezone_name_from_offset(offset) ]
+  end
+
+  def get_timezone_name_from_offset(offset = 0)
+    ActiveSupport::TimeZone::MAPPING.keys.find { |tz_name|
+      ActiveSupport::TimeZone[tz_name].utc_offset == 3600 * offset
+    }
   end
 end
--- app/views/default/parse.json.jbuilder
+++ app/views/default/parse.json.jbuilder
@@ -1,3 +1,6 @@
 json.q   @q
 json.utc @utc
+json.offset @offset
+json.local @local
+json.timezone @timezone
 
