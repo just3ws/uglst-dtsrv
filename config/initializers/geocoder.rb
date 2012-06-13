64b97e44a9dc484fc2ad150f582e1c1a79f2cdbb
Michael D. Hall <mdh@just3ws.com>
2012-06-13 08:43:30 -0500

    disabled redis cache

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -14,6 +14,6 @@ Geocoder.configure do |config|
   #config.units = :km
 
   # caching (see below for details):
-  config.cache = Redis.new
+  #config.cache = Redis.new
   #config.cache_prefix = "..."
 end
